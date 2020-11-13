6aa72baac628dd6fac24e114b128fe99902d14e5
Aleksey <aleks-herou@mail.ru>
2020-11-13 16:49:32 +0300

    Внедрение компонента ToDoItem в разметку

--- react_spa/src/components/Main.js
+++ react_spa/src/components/Main.js
@@ -1,33 +1,36 @@
 import React, { Component } from "react";
-import {
-    Route,
-    NavLink,
-    HashRouter
-  } from "react-router-dom";
+import
+    {
+        Route,
+        NavLink,
+        HashRouter
+    } from "react-router-dom";
 import Home from "./Home";
 import ToDos from "./ToDos";
 import Contact from "./Contact";
+import ToDoItem from "./ToDoItem";
  
-class Main extends Component {
-  render() {
-    return (
-        <HashRouter>
+class Main extends Component
+{
+    render()
+    {
+        return <HashRouter>
             <div>
-            <h1>SPA ToDo App</h1>
-            <ul className="header">
-                <li><NavLink exact to="/">Главная</NavLink></li>
-                <li><NavLink to="/todos">ToDos</NavLink></li>
-                <li><NavLink to="/contact">Контакты</NavLink></li>
-            </ul>
-            <div className="content">
-                <Route exact path="/" component={Home}/>
-                <Route path="/todos" component={ToDos}/>
-                <Route path="/contact" component={Contact}/>
+                <h1>SPA ToDo App</h1>
+                <ul className="header">
+                    <li><NavLink exact to="/">Главная</NavLink></li>
+                    <li><NavLink to="/todos">ToDos</NavLink></li>
+                    <li><NavLink to="/contact">Контакты</NavLink></li>
+                </ul>
+                <div className="content">
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/todos" component={ToDos}/>
+                    <Route exact path="/todos/:id(\d+)" component={ToDoItem}/>
+                    <Route path="/contact" component={Contact}/>
+                </div>
             </div>
-            </div>
-        </HashRouter>
-    );
-  }
+        </HashRouter>;
+    }
 }
  
 export default Main;
